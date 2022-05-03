0bffdb47c2e57d050cd0b6243c9659d61141a313
Simone <cirino.simone99@gmail.com>
2022-05-03 14:01:16 +0200

    finito file js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
     Creare un generatore di password partendo da dei dati forniti dall'utente
 */
 
+const nome = prompt('Come ti chiami?');
+console.log(nome);
+const cognome = prompt('Qual è il tuo cognome?');
+console.log(cognome);
+const colore = prompt('Qual è il tuo colore preferito?');
+console.log(colore);
+const password = nome + cognome + colore + '21';
+console.log(password);
+document.getElementById('pw').innerHTML = `La tua password è: ${password}`;
